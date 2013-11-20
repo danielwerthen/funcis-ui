0d3145de9bd83a83456bbe9939783ad0a9d749e2
Daniel Werthén <daniel.werthen@netlight.com>
2013-11-20 20:29:30 +0100

    Funcis up and away

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-var UI = require('./ui'),
-	ui = new UI();
+var funcis = require('funcis'),
+	app = funcis(),
+	uiNode = require('./ui-node').register(app);
 
-ui.on('socket', function (id) {
-	ui.bind('#search', 'click', {}, id, function () {
-		ui.render('views/search.jade', {}, '#main', id);
-		setTimeout(function () {
-		ui.bind('#search-box', 'change', {}, id, function (sel, val, attrs) {
-			ui.html(val, '#search-result', id);
-		});
-		}, 500);
-	});
-	ui.bind('#settings', 'click', {}, id, function () {
-		ui.render('views/settings.jade', {}, '#main', id);
-	});
-});
+
+app.script('init');
+
---
+++ ui-node.js
@@ -0,0 +1,77 @@
+var UI = require('./ui'),
+	_ = require('underscore'),
+	funcis = require('funcis'),
+	onConnections = [],
+	ui = new UI();
+
+
+
+ui.on('socket', function (id) {
+	for (var i in onConnections) {
+		onConnections[i](id);
+	}
+	ui.bind('#search', 'click', {}, id, function () {
+		ui.render('views/search.jade', {}, '#main', id);
+		setTimeout(function () {
+		ui.bind('#search-box', 'change', {}, id, function (sel, val, attrs) {
+			ui.html(val, '#search-result', id);
+		});
+		}, 500);
+	});
+	ui.bind('#settings', 'click', {}, id, function () {
+		ui.render('views/settings.jade', {}, '#main', id);
+	});
+});
+
+module.exports = {
+	register: function (app) {
+		var uiNode = app.node('UI');
+		uiNode.functions.add('OnConnection', function (next) {
+			onConnections.push(next);
+		});
+
+		uiNode.functions.add('Render', function (view, options, selector, id, next) {
+			if (_.isFunction(id)) {
+				next = id;
+				id = null;
+			}
+			if (!!id) {
+				ui.render(view, {}, selector, id);
+			} else {
+				ui.render(view, {}, selector);
+			}
+			next();
+		});
+
+		uiNode.functions.add('Bind', function (selector, events, attrs, id, next) {
+			if (_.isFunction(id)) {
+				next = id;
+				id = null;
+			}
+			ui.bind(selector, events, attrs, id, function (sel, val, attrs) {
+				next(sel, val, attrs);
+			});
+		});
+
+		uiNode.functions.add('Html', function (selector, value, id, next) {
+			if (_.isFunction(id)) {
+				next = id;
+				id = null;
+			}
+			ui.html(value, selector, id);
+			next();
+		});
+
+
+		uiNode.functions.add('Val', function (selector, id, next) {
+			if (_.isFunction(id)) {
+				next = id;
+				id = null;
+			}
+			ui.val(selector, id, function (val) {
+				next(val);
+			});
+		});
+		return uiNode;
+	}
+};
